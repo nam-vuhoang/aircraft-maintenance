f6f67284f595001162fd3ec88035a72e3679496a
Nam Vu <nam.vuhoang@gmail.com>
2024-07-07 23:44:17 +0300

    Declare missing types

---
+++ frontend/src/@types/missing-types.d.ts
@@ -0,0 +1,8 @@
+/**
+ * This file is used to declare types for modules that don't have types available.
+ */
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+declare module 'react-resizable' {
+  export const ResizableBox: any;
+}
