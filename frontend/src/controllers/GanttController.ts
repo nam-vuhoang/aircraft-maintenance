3408f078adb41a4d4dadbd09dc720ca5220ad360
Nam Vu <nam.vuhoang@gmail.com>
2024-07-07 23:55:30 +0300

    Remove redundant files

--- frontend/src/controllers/GanttController.ts
+++
@@ -1,7 +0,0 @@
-import { GanttModel } from '../models/GanttModel';
-
-export const fetchGanttData = async () => {
-  // Replace with actual API call
-  const data = await GanttModel.getData();
-  return data;
-};
