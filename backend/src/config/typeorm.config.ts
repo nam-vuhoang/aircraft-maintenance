3789334b52a6b9cdd5e8203ddb34b5632b11ef42
Nam Vu <nam.vuhoang@gmail.com>
2024-07-10 15:39:02 +0300

    Throw error when config undefined

--- backend/src/config/typeorm.config.ts
+++ backend/src/config/typeorm.config.ts
@@ -1,16 +1,37 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { config } from 'dotenv';
+import * as path from 'path';
 
-config();
+config({ path: path.resolve(__dirname, '../../.env') });
+
+function validateEnvVariable(variable: string, variableName: string): string {
+  if (!variable) {
+    console.error(
+      `Error: ${variableName} is not defined. Please set ${variableName} in your environment variables.`,
+    );
+    throw new Error(
+      `Configuration error: ${variableName} is not defined. Please set ${variableName} in your environment variables.`,
+    );
+  }
+  return variable;
+}
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.DATABASE_HOST,
-  port: parseInt(process.env.DATABASE_PORT, 5432),
-  username: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
+  host: validateEnvVariable(process.env.DATABASE_HOST, 'DATABASE_HOST'),
+  port: parseInt(
+    validateEnvVariable(process.env.DATABASE_PORT, 'DATABASE_PORT'),
+    10,
+  ),
+  username: validateEnvVariable(process.env.DATABASE_USER, 'DATABASE_USER'),
+  password: validateEnvVariable(
+    process.env.DATABASE_PASSWORD,
+    'DATABASE_PASSWORD',
+  ),
+  database: validateEnvVariable(process.env.DATABASE_NAME, 'DATABASE_NAME'),
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-  synchronize: true, // Not recommended for production
-  logging: true,
+  synchronize: !isProduction, // Only synchronize in development
+  logging: !isProduction, // Enable logging only in development
 };
