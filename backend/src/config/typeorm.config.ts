7467f33fce8c7e988a22cdc8f35b0e4f95a8ad3f
Nam Vu <nam.vuhoang@gmail.com>
2024-07-10 19:13:51 +0300

    Setup backend log level

--- backend/src/config/typeorm.config.ts
+++ backend/src/config/typeorm.config.ts
@@ -1,6 +1,7 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { config } from 'dotenv';
 import * as path from 'path';
+import { LogLevel } from 'typeorm';
 
 config({ path: path.resolve(__dirname, '../../.env') });
 
@@ -18,6 +19,45 @@ function validateEnvVariable(variable: string, variableName: string): string {
 
 const isProduction = process.env.NODE_ENV === 'production';
 
+/**
+ * Get the TypeORM log levels based on the base log level
+ * @param baseLogLevel The base log level, e.g. 'debug', 'info', 'warn', 'error'
+ * @returns An array of TypeORM log levels
+ */
+const getTypeORMLogLevels = (): LogLevel[] => {
+  const baseLogLevel = process.env.LOG_LEVEL || 'info';
+
+  // LogLevel = "query" | "schema" | "error" | "warn" | "info" | "log" | "migration"
+  const logLevels: LogLevel[] = [];
+
+  switch (baseLogLevel) {
+    case 'debug':
+      logLevels.push(
+        'query',
+        'schema',
+        'error',
+        'warn',
+        'info',
+        'log',
+        'migration',
+      );
+      break;
+    case 'info':
+      logLevels.push('info', 'warn', 'error', 'log');
+      break;
+    case 'warn':
+      logLevels.push('warn', 'error');
+      break;
+    case 'error':
+      logLevels.push('error');
+      break;
+    default:
+      logLevels.push('error', 'warn', 'info', 'log');
+  }
+
+  return logLevels;
+};
+
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: validateEnvVariable(process.env.DATABASE_HOST, 'DATABASE_HOST'),
@@ -33,5 +73,5 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   database: validateEnvVariable(process.env.DATABASE_NAME, 'DATABASE_NAME'),
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: !isProduction, // Only synchronize in development
-  logging: !isProduction, // Enable logging only in development
+  logging: getTypeORMLogLevels(),
 };
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,17 +1,55 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, LogLevel, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+
+/**
+ * Get the log levels based on the base log level
+ * @param baseLogLevel The base log level, e.g. 'debug', 'info', 'warn', 'error'
+ * @returns 
+ */
+const getNestLogLevels = (baseLogLevel: string): LogLevel[] => {
+  // https://docs.nestjs.com/techniques/logger
+  // type LogLevel = 'log' | 'error' | 'warn' | 'debug' | 'verbose' | 'fatal';
+  const logLevels: LogLevel[] = [];
+
+  switch (baseLogLevel) {
+    case 'debug':
+      logLevels.push('debug', 'log', 'warn', 'error', 'verbose');
+      break;
+    case 'info':
+      logLevels.push('log', 'warn', 'error');
+      break;
+    case 'warn':
+      logLevels.push('warn', 'error');
+      break;
+    case 'error':
+      logLevels.push('error');
+      break;
+    default:
+      logLevels.push('log', 'warn', 'error');
+  }
+
+  return logLevels;
+};
 
 async function bootstrap() {
   // Create the Nest application
   const app = await NestFactory.create(AppModule);
 
+  // Enable logging
+  const logLevel = process.env.LOG_LEVEL || 'info';
+  app.useLogger(getNestLogLevels(logLevel));
+
+  const logger = new Logger('bootstrap');
+  logger.log('Logger level:', logLevel);
+
   // Enable CORS for development
   const configService = app.get(ConfigService);
   console.log('NODE_ENV:', configService.get<string>('NODE_ENV'));
   if (configService.get<string>('NODE_ENV') === 'development') {
+    logger.warn('Enabling CORS for development');
     app.enableCors({
       origin: configService.get<string>('FRONTEND_URL'),
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
@@ -30,6 +68,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
+  logger.log('Swagger documentation available at /api-docs');
 
   // Start the application
   await app.listen(3000);
